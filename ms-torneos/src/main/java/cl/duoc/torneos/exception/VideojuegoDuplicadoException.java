ba3d6ac9d0ae8d99c862f9a29ac4234c406d2db0
esmartineza-spec <es.martineza@duocuc.cl>
2026-05-06 08:39:10 -0400

    Generando microservicio Torneo y actualizando Microservicio de Juegos

--- ms-torneos/src/main/java/cl/duoc/torneos/exception/VideojuegoDuplicadoException.java
+++
@@ -1,8 +0,0 @@
-package cl.duoc.torneos.exception;
-
-public class VideojuegoDuplicadoException extends RuntimeException {
-    public VideojuegoDuplicadoException(String nom_torneo, String videojuego) {
-        super("Ya hay registro de un torneo de nombre: "+nom_torneo+ " con el videojuego: "+videojuego);
-    }
-
-}
