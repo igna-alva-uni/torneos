86a00b77f24c5b75c63c8c7bae3436cae39ef36f
esmartineza-spec <es.martineza@duocuc.cl>
2026-05-15 10:08:57 -0400

    Validacion DTO Torneo

--- ms-torneos/src/main/java/cl/duoc/torneos/dto/TorneosRequest.java
+++ ms-torneos/src/main/java/cl/duoc/torneos/dto/TorneosRequest.java
@@ -2,14 +2,19 @@ package cl.duoc.torneos.dto;
 
 import cl.duoc.torneos.model.Formato;
 import cl.duoc.torneos.model.Premio;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
+import lombok.NonNull;
 
 import java.time.LocalDate;
 
 @Data
 public class TorneosRequest {
+    @NotBlank
     private String nombre;
+    @NonNull
     private Integer idJuego;
+    @NonNull
     private Integer idFormato;
     private LocalDate fechaInicio;
     private LocalDate fechaTermino;
