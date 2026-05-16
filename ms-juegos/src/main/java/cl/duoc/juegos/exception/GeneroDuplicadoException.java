436c73a6c977f3925e7abf9dadd6e151c5f6a6db
igna-alva-uni <igna.alvarezc@duocuc.cl>
2026-05-16 17:04:06 -0400

    deleted un used files from branch

--- ms-juegos/src/main/java/cl/duoc/juegos/exception/GeneroDuplicadoException.java
+++
@@ -1,7 +0,0 @@
-package cl.duoc.juegos.exception;
-
-public class GeneroDuplicadoException extends RuntimeException{
-    public GeneroDuplicadoException(String genero, String nombre){
-        super("Ya existe un videojuego registrado con el genero: "+genero+", con el nombre de: "+nombre);
-    }
-}
