701b4bc1e94d1d879dca36688590805a4a80656b
igna-alva-uni <igna.alvarezc@duocuc.cl>
2026-06-25 23:31:03 -0400

    ms-usuarios: updated main application class

--- ms-usuarios/src/main/java/cl/duoc/usuarios/TorneosUsuariosApplication.java
+++ ms-usuarios/src/main/java/cl/duoc/usuarios/TorneosUsuariosApplication.java
@@ -3,11 +3,21 @@ package cl.duoc.usuarios;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"cl.duoc.usuarios", "cl.duoc.commons"})
 public class TorneosUsuariosApplication {
 
 	public static void main(String[] args) {
+		int defaultPort = 9001;
+		int resolvedPort = isPortAvailable(defaultPort) ? defaultPort : 0;
+		System.setProperty("server.port", String.valueOf(resolvedPort));
 		SpringApplication.run(TorneosUsuariosApplication.class, args);
 	}
 
+	private static boolean isPortAvailable(int port) {
+		try (java.net.ServerSocket serverSocket = new java.net.ServerSocket(port)) {
+			return true;
+		} catch (java.io.IOException e) {
+			return false;
+		}
+	}
 }
