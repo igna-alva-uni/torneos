23764d223132341c60ed078d7385667d1ec91a7e
igna-alva-uni <igna.alvarezc@duocuc.cl>
2026-06-25 23:29:23 -0400

    ms-juegos: added SecurityConfig and JWT auth integration

--- ms-juegos/src/main/java/cl/duoc/juegos/TorneosJuegosApplication.java
+++ ms-juegos/src/main/java/cl/duoc/juegos/TorneosJuegosApplication.java
@@ -3,11 +3,21 @@ package cl.duoc.juegos;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"cl.duoc.juegos", "cl.duoc.commons"})
 public class TorneosJuegosApplication {
 
 	public static void main(String[] args) {
+		int defaultPort = 9003;
+		int resolvedPort = isPortAvailable(defaultPort) ? defaultPort : 0;
+		System.setProperty("server.port", String.valueOf(resolvedPort));
 		SpringApplication.run(TorneosJuegosApplication.class, args);
 	}
 
+	private static boolean isPortAvailable(int port) {
+		try (java.net.ServerSocket serverSocket = new java.net.ServerSocket(port)) {
+			return true;
+		} catch (java.io.IOException e) {
+			return false;
+		}
+	}
 }
