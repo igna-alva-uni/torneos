27de33a38fb63fd8be373b28ab986b9d3b190f70
igna-alva-uni <igna.alvarezc@duocuc.cl>
2026-06-25 23:30:17 -0400

    ms-torneos: added SecurityConfig and JWT auth integration

--- ms-torneos/src/main/java/cl/duoc/torneos/TorneosTorneosApplication.java
+++ ms-torneos/src/main/java/cl/duoc/torneos/TorneosTorneosApplication.java
@@ -5,11 +5,21 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableFeignClients(basePackages = "cl.duoc")
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"cl.duoc.torneos", "cl.duoc.commons"})
 public class TorneosTorneosApplication {
 
 	public static void main(String[] args) {
+		int defaultPort = 9004;
+		int resolvedPort = isPortAvailable(defaultPort) ? defaultPort : 0;
+		System.setProperty("server.port", String.valueOf(resolvedPort));
 		SpringApplication.run(TorneosTorneosApplication.class, args);
 	}
 
+	private static boolean isPortAvailable(int port) {
+		try (java.net.ServerSocket serverSocket = new java.net.ServerSocket(port)) {
+			return true;
+		} catch (java.io.IOException e) {
+			return false;
+		}
+	}
 }
