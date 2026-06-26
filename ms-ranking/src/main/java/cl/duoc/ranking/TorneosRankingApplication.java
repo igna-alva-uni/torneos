295b5cfa521f4b1bb9e27fa96c6c480e43b59b8a
igna-alva-uni <igna.alvarezc@duocuc.cl>
2026-06-25 23:30:06 -0400

    ms-ranking: added SecurityConfig and JWT auth integration

--- ms-ranking/src/main/java/cl/duoc/ranking/TorneosRankingApplication.java
+++ ms-ranking/src/main/java/cl/duoc/ranking/TorneosRankingApplication.java
@@ -7,11 +7,21 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableDiscoveryClient
 @EnableFeignClients(basePackages = "cl.duoc")
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"cl.duoc.ranking", "cl.duoc.commons"})
 public class TorneosRankingApplication {
 
 	public static void main(String[] args) {
+		int defaultPort = 9008;
+		int resolvedPort = isPortAvailable(defaultPort) ? defaultPort : 0;
+		System.setProperty("server.port", String.valueOf(resolvedPort));
 		SpringApplication.run(TorneosRankingApplication.class, args);
 	}
 
+	private static boolean isPortAvailable(int port) {
+		try (java.net.ServerSocket serverSocket = new java.net.ServerSocket(port)) {
+			return true;
+		} catch (java.io.IOException e) {
+			return false;
+		}
+	}
 }
